58c2a11e6ac2a75d41b2dee63b34da4291f5d404
Воронцов Илья Викторович <Nebmes@mail.ru>
2022-06-21 15:39:03 +0300

    add HomeWork

---
+++ Task2/Program.cs
@@ -0,0 +1,75 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+Console.Clear();
+
+int RandomNumbers(int numberElements, int min, int max)
+{
+int[] randomNumbers = new int[numberElements];
+int sumElements = 0;
+Console.Write("Получившийся массив: ");
+
+for (int i = 0; i <randomNumbers.Length; i++ )
+{
+    randomNumbers[i] = new Random().Next(min, max);
+
+    Console.Write(randomNumbers[i] + " ");
+
+    if (i % 2 != 1)
+    {
+    sumElements = sumElements + randomNumbers[i];
+    }
+}
+    return sumElements;
+}
+
+Console.Write($"Введи количество элементов массива: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
+Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
+
+// или
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string num = Console.ReadLine();
+    return int.Parse(num);
+}
+void FillArray(int[] array)
+{
+  for (int i = 0; i<array.Length; i++ )
+  {
+    array[i] = new Random().Next(1, 100);
+  }
+}
+void WriteArray(int[] array)
+{
+  for (int i = 0; i<array.Length; i++ )
+  {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+int SumOddElement(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 1)
+        {
+        sum = sum + array[i];
+        }
+    }
+    return sum;
+}
+
+int num = Prompt("Введите количество элементов массива: ");
+int[] numbers = new int[num];
+FillArray(numbers);
+WriteArray(numbers);
+SumOddElement(numbers);
+System.Console.WriteLine("Сумма элементов, стоящих на нечётных позициях = " +SumOddElement(numbers));
---
+++ Task3/Program.cs
@@ -0,0 +1,72 @@
+﻿
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+
+int[] MyArray = new int[10];
+for (int i = 0; i < MyArray.Length; i++ )
+  {
+    MyArray[i] = new Random().Next(1, 10);
+    Console.Write(MyArray[i] + " ");
+  }
+
+int maxNumber = MyArray[0];
+int minNumber = MyArray[0];
+
+  for (int i = 1; i < MyArray.Length; i++)
+  {
+    if (maxNumber < MyArray[i])
+    {
+      maxNumber = MyArray[i];
+    }
+        if (minNumber > MyArray[i])
+    {
+      minNumber = MyArray[i];
+    }
+  }
+
+int result = maxNumber - minNumber;
+
+Console.WriteLine($"\nразница между между максимальным ({maxNumber}) и минимальным ({minNumber}) элементами: {result}");
+
+// или
+
+void FillArray(int[] array)
+{
+  for (int i = 0; i<array.Length; i++ )
+  {
+    array[i] = new Random().Next(1, 100);
+  }
+}
+void WriteArray(int[] array)
+{
+  for (int i = 0; i<array.Length; i++ )
+  {
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+int DifferenceOfElements(int[] array)
+{
+    int maxNumber = array[0];
+    int minNumber = array[0];
+    for (int i = 1; i < array.Length; i++)
+        {
+        if (maxNumber < array[i])
+        {
+        maxNumber = array[i];
+        }
+        if (minNumber > array[i])
+        {
+        minNumber = array[i];
+        }
+    }
+    int result = maxNumber - minNumber;
+    return result;  
+}
+
+int[] numbers = new int[10];
+FillArray(numbers);
+WriteArray(numbers);
+DifferenceOfElements(numbers);
+System.Console.WriteLine("Разница между минимальным и максимальным элементами = "+DifferenceOfElements(numbers));
